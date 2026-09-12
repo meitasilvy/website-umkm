24f6b474804cfc9a291d2e600013e16b4a0d7452
meitasilvy <meitasilvianasr@gmail.com>
2026-09-12 22:00:25 +0700

    feat: sesuaikan promo Risol Gurih

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+
+    promoButton.textContent = "Promo: Beli 2 gratis 1!";
+    
+    console.log("Promo Risol Gurih berhasil ditampilkan.");
+
 });
